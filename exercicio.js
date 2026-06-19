5b378393924250eb2a812adb29d1f36a8c78d013
guimaraesvazana-blip <guimaraes.vaz.ana@escola.pr.gov.br>
2026-06-19 11:41:23 +0000

    Meu primeiro código

---
+++ exercicio.js
@@ -0,0 +1,6 @@
+/* Crie uma variável para armazenar a quantidade de vidas que um jogador tem em um jogo (começando com
+3 vidas). Certifique-se de escolher o tipo de dado correto. */
+
+let nomeAluno = "Ana"
+let idadeAluno = "16"
+console.log ("Meu nome é", nomeAluno);
