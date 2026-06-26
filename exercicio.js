0d31570a47d51181a5e73209871ce73338256a65
guimaraesvazana-blip <guimaraes.vaz.ana@escola.pr.gov.br>
2026-06-26 11:40:12 +0000

    modifiquei

--- exercicio.js
+++ exercicio.js
@@ -1,6 +1,10 @@
 /* Crie uma variável para armazenar a quantidade de vidas que um jogador tem em um jogo (começando com
 3 vidas). Certifique-se de escolher o tipo de dado correto. */
-
-let nomeAluno = "Ana"
-let idadeAluno = "16"
-console.log ("Meu nome é", nomeAluno);
+let vida = 3;
+/*Declare uma variável para guardar o nome de um herói de quadrinhos. Logo abaixo, escreva a linha de
+código que altera o nome desse herói para outro de sua preferência.*/
+let nomeHerói = "Homem de ferro";
+nomeHerói = "Homem aranha";
+/*Um usuário acabou de fazer login em um aplicativo. Crie uma variável do tipo Boolean que guarde essa
+informação (indique que ele está logado).*/
+let estaLogado = true
